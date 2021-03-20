860fd845d4ca09e928ba36b10de3cbc2469eab0a
Ali Almasli <ali.almasli@accessbank.az>
2021-03-20 10:23:56 +0400

    Initial Commit

---
+++ payday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  payday
+//
+//  Created by AccessBank 2 on 20.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
